9897c02beb3157348ec294c6aac2425d8589fe38
nivetha-dotone <nivetha.mohansingh@dot1.in>
2024-10-07 16:04:32 +0530

    removing

--- CWFM/src/main/java/com/wfd/dot1/cwfm/dao/MasterUserDao.java
+++
@@ -1,8 +0,0 @@
-package com.wfd.dot1.cwfm.dao;
-
-import com.wfd.dot1.cwfm.pojo.MasterUser;
-
-public interface MasterUserDao {
-
-	public MasterUser findMasterUserDetailsByUserName(String username,String password);
-}
