4d6e032f37857479e14e58078caf74301a728955
nivetha-dotone <nivetha.mohansingh@dot1.in>
2024-10-08 11:24:51 +0530

    reverting

--- CWFM/src/main/java/com/wfd/dot1/cwfm/service/MasterUserService.java
+++
@@ -1,8 +0,0 @@
-package com.wfd.dot1.cwfm.service;
-
-import com.wfd.dot1.cwfm.pojo.MasterUser;
-
-public interface MasterUserService {
-
-	public MasterUser findMasterUserDetailsByUserName(String username,String password);
-}
